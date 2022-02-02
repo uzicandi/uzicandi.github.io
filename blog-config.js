3434740eeaf84edc23a9f242305bedd5b748c07f
Yona <uzicandi@gmail.com>
2022-02-02 15:38:57 +0900

    first blog

---
+++ blog-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: "ocean.dev",
+  description: "Front-end Engineer @Class101",
+  author: "ocean",
+  siteUrl: "https://uzicandi.github.io",
+  links: {
+    github: "https://github.com/uzicandi",
+    linkedIn: "https://www.linkedin.com/in/jiwoo-kim-b876a9123/",
+    email: "mailto:kys7196@gmail.com"
+  },
+  utterances: {
+    repo: "uzicandi/uzicandi.github.io",
+    type: "pathname"
+  }
+}
