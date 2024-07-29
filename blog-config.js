aa6191b92f4abbd5485b9c601ed3893aa3d13d15
uzicandi <uzicandi@gmail.com>
2024-07-29 15:43:54 +0900

    new blog

---
+++ blog-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: "ocean.dev",
+  description: "front-end developer",
+  author: "ocean",
+  siteUrl: "https://uzicandi.github.io",
+  links: {
+    github: "https://github.com/uzicandi",
+    linkedIn: "https://www.linkedin.com/in/jiwoo-kim-b876a9123/",
+    email: "mailto:kys7196@gmail.com",
+    resume: "https://deciduous-bottle-038.notion.site/Jiwoo-Kim-8d73f82ff3cb41888d0fb8aa96ce4800"
+  },
+  utterances: {
+    repo: "uzicandi/uzicandi.github.io",
+    type: "pathname"
+  }
+}
